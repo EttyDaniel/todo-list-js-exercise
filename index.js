0c8dfc876890474ebfa33a57b8a9bd648031b7fb
Eti Daniel <danieleti@gmail.com>
2021-08-09 11:21:26 -0400

    index.js refactor

--- index.js
+++ index.js
@@ -8,20 +8,15 @@ function newTask(title, description) {
   const task = {
     title: title,
     description: description,
-    complete: false
+    complete: false,
+    logState: function() {
+      console.log(`${this.title} has${this.complete ? " " : " not "}been completed`);
+    },
+    markCompleted: function() {
+      this.complete = true;
+    }
   };
   return task;
-}
-
-// Mark a task as complete by setting the task's status in the `taskComplete` array to `true`
-function completeTask(taskIndex) {
-
-}
-
-// Print the state of a task to the console in a nice readable way
-function logTaskState(taskIndex) {
-
-  //console.log(`${title} has${complete ? " " : " not "}been completed`);
 }
 
 // DRIVER CODE BELOW
@@ -29,4 +24,6 @@ function logTaskState(taskIndex) {
 const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter box");
 const task2 = newTask("Do Laundry", "😨");
 const tasks = [task1, task2];
-console.log(tasks);
+task1.logState();
+task1.markCompleted();
+task1.logState();
